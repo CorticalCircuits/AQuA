789466deb40d7327423b35108e94853fa7845d4d
CarlosFIU <47358972+CarlosFIU@users.noreply.github.com>
2020-07-01 09:55:06 -0400

    AstroVidMaker + BatchEventVidMaker
    Creates a video of astrocyte events , use BatchEventVidMaker are read comments on how to use for multiple mat versus single mat

---
+++ post_AQuA/AstroVidMaker.m
@@ -0,0 +1,80 @@
+tic
+%% Selecting Landmark and Invading Event Index (If running non batch here make sure to uncomment this section)
+% whicchLandmark = 1;
+% whichevt = 3;
+% % figure;plot(res.ftsFilter.region.landmarkDir.chgTowardBefReach)
+
+%% Direction Score Threshold + mean image + soma border
+thresh = 50;
+mnIm = mean(res.datOrg,3);
+somaBorder = res.fts.region.landMark.border{whicchLandmark};
+%% Indexing all invading Events
+% This creates a new index of all the events that invade the soma with a
+% value greater than the set threshold above
+invadingEvents = find(res.fts.region.landmarkDir.chgToward(:,whicchLandmark)>thresh);
+%% Text on figure
+%Here below is the use of string in a pre than post process in order to
+%refrence the variables from AQUA and then print those feature values into
+%the movie that will be played
+PreText = {string(res.opts.fileName),"Event #: %.0f ","Area of Event: %.1f  mu^2", "Frames: %.0f " , "Time: %.1f s ", "Direction Score Towards: %.1f", "Direction Score Away: %.1f"};% Need to add mu ^2 to the area was having issues with the printing but this is a minor issue as we can just add a mu in text in the plot window
+A(1) = string(res.opts.fileName)
+A(2) = whichevt
+A(3) = (res.fts.basic.area(invadingEvents(whichevt))); %Area
+A(4) = (res.fts.loc.t1(invadingEvents(whichevt)) - res.fts.loc.t0(invadingEvents(whichevt))) % Frames
+A(5) = (res.fts.loc.t1(invadingEvents(whichevt)) - res.fts.loc.t0(invadingEvents(whichevt)))/(res.opts.frameRate)
+A(6) = (res.fts.region.landmarkDir.chgToward(invadingEvents(whichevt)));
+A(7) = (res.fts.region.landmarkDir.chgAway(invadingEvents(whichevt)));
+for n = 1:1:(numel(A(:)))  
+charText{n} = sprintf(PreText{n},(A(n)));
+PostText = convertCharsToStrings(charText);
+end
+
+vidname = charText(1)+"Event"+string((invadingEvents(whichevt)));
+VidNombre = convertStringsToChars(vidname);
+
+%% Video Writer
+%This is the video writer code that is saved in the form of an .avi file. I
+%attempted to make this into a mp4 howver when I do this the video does not
+%really save as well and is not the same speed as the original shown in the
+%plot
+%v = VideoWriter(PostText(1)+"Event#:"+string((invadingEvents(whichevt)))+".avi");
+v = VideoWriter(VidNombre,'MPEG-4');
+v.FrameRate = 8;
+v.Quality = 100;
+open(v)
+%% Plot Creation
+fig = figure(1)
+preIm = zeros([250 256]);
+x = imshow(preIm)
+hold on 
+set(fig, 'Position', get(0, 'Screensize'));
+plot(somaBorder(:,2),somaBorder(:,1),'.')
+hold on
+%% Legend Manipulation
+plot(somaBorder(:,2),somaBorder(:,1),'-')
+hold on
+for whichEvt = whichevt%1:length(invadingEvents)
+    temp = zeros(size(res.datOrg));
+    temp(res.evt{invadingEvents(whichEvt)}) = 1;
+%     figure
+%     imagesc(sum(temp,3))
+%     hold on
+%     plot(somaBorder(:,2),somaBorder(:,1),'o')
+%     colormap hot
+    clear M
+    ind = 0;
+    for t = res.fts.curve.tBegin(invadingEvents(whichEvt)):res.fts.curve.tEnd(invadingEvents(whichEvt))
+        ind = ind+1;
+%         imagesc(temp(:,:,t));
+      overlayImages(mnIm, temp(:,:,t),  temp(:,:,t),.6,'hot',[0 1])
+             text(.15,.15,PostText,'Color','w','Units','Normalized')
+             hold on
+             plot(somaBorder(:,2),somaBorder(:,1),'.')
+   M(ind) = getframe(fig)
+   
+    end
+
+end
+   writeVideo(v,M);
+close(v)
+toc
---
+++ post_AQuA/BatchEventVidMaker.m
@@ -0,0 +1,22 @@
+%% Path to Res file of Cell
+paths = uipickfiles;
+
+%% Selecting Landmark and Invading Event Index
+whicchLandmark = 1;%Set as Soma to define the direction scores
+whichevt = [1:50]; %Events to visualize, if want multiple for one mat file then put the array here of evt #
+forlooper = numel(whichevt)
+% %% Vid maker for loop set for multiple mat files
+% for n = 1:numel(paths)
+%     whichevt = whichevt(n) %% if multiple events for one mat file then change numel(paths) to numel(whichevt)
+%     load(paths{n}) %% Comment this out and load single res file if only working with one mat file
+%     AstroVidMaker
+%     close all
+% end
+
+%% Vid maker for loop set for single mat files
+for n = 1:forlooper
+load(paths{1})
+    whichevt = n %% if multiple events for one mat file then change numel(paths) to numel(whichevt)
+    AstroVidMaker
+    close all
+end

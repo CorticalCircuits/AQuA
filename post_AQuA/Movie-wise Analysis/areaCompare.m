e8eaf8aec120c49c711e1c06d49110426e0d8b19
TomSuarezO <tsuar027@fiu.edu>
2020-12-04 12:00:10 -0500

    Update areaCompare
    Optimization of excessively large variable vidFrame. Code runs significantly faster

--- post_AQuA/Movie-wise Analysis/areaCompare.m
+++ post_AQuA/Movie-wise Analysis/areaCompare.m
@@ -12,32 +12,43 @@ if ~exist('whichevt','var')
 end
 
 %% Area Calculation
+%Calculates number of shared pixels shared between the trace of an event
+%and a frame with only true values (trueFrame) within the borders of
+%landmark
 count = 1;
+landMk = bd0{1}{1}{1};
+frameSize = [size(res.datOrg,1) , size(res.datOrg,2)];
+frameElement = frameSize(1)*frameSize(2);
 for ii = whichevt
-    %Traces
-    vidFrames = false(size(res.datOrg));
-    vidFrames(res.evt{(ii)}) = true;
-    trace = logical(sum(vidFrames,3));
-    landMk = bd0{1}{1}{1};
+    %Find trace of Event
+    firstFrame = floor(min(res.evt{(ii)})/frameElement); %First frame of event in movie
+    lastFrame = ceil(max(res.evt{(ii)})/frameElement); %Last frame of event in movie
+    vidFrames = false(frameSize(1) , frameSize(2) , lastFrame-firstFrame);
+    vidFrames(res.evt{(ii)} - firstFrame*frameElement) = true; %Binary movie of event
+    trace = logical(sum(vidFrames,3)); %Trace of event (one frame)
     %Area
-    [yInd , xInd] = find(trace == true);
+    [yInd , xInd] = find(trace == true); %x,y indexes of active pixels
     in = inpolygon(xInd,yInd,landMk(:,2),landMk(:,1));
-    trueFrame = true(size(trace));
+    trueFrame = true(size(trace));%dummy variable for true comparison
     [yTrue , xTrue] = find(trueFrame == true);
     pin = inpolygon(yTrue,xTrue,landMk(:,2),landMk(:,1));
-    coveredSomaArea(count) = 100 * numel(find(in==true))/numel(find(pin==true));
-    insideSomaArea(count) = 100 * numel(find(in==true))/numel(find(trace==true));
+    coveredSomaArea(count) = 100 * numel(find(in==true))/numel(find(pin==true)); %Area of soma covered by an event
+    insideSomaArea(count) = 100 * numel(find(in==true))/numel(find(trace==true)); %Area of event inside the soma
     count = count+1;
 end
 
-%% Ratio Generation
-significantEvents = all([insideSomaArea>90;res.fts.basic.area<200;coveredSomaArea>2]);
-ratio = sum(significantEvents)/sum(all([res.fts.basic.area<200;coveredSomaArea>2]));
+%% Ratio Calculation
+% Ratio of yellow events (completely inside soma)smaller than 200um2 divided
+% by all events under 200um2
+if oneEvent
+    significantEvents = all([insideSomaArea>90;res.fts.basic.area<200;coveredSomaArea>2]);
+    ratio = sum(significantEvents)/sum(all([res.fts.basic.area<200;coveredSomaArea>2]));
+end
 
 %% Figure Display
 fig = figure;
 ax = axes('parent',fig);
-if oneEvent
+if oneEvent %Display only one event and the soma border
     fprintf('Percentage of Soma Covered = %2.2f%%\nPercentage of Trace in Soma = %2.2f%%\n',...
         coveredSomaArea,insideSomaArea);
     outText = sprintf('Percentage of Soma Covered = %2.2f%%\nPercentage of Event inside Soma = %2.2f%%\n',...
@@ -50,7 +61,8 @@ if oneEvent
     hold(ax,'off');
     text(.05,.05,outText,'Color','w','Units','Normalized');
     title(['Event #',num2str(whichevt),' Trace']);
-else
+    ratio = [];
+else %Display chart with colors and calculate ratio    
     h = parula;
     colormap parula
     color = ceil(63*(insideSomaArea/100))+1;

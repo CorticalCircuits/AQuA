3bb596855a004ec5bd07e2ac1a16d87692d5261a
CarlosFIU <47358972+CarlosFIU@users.noreply.github.com>
2022-06-13 12:29:48 -0400

    Update for Area Compare and  Adding Hot Spot Max
    The area compare code was updated to use binary intersection comparison for determining the soma ratio rather than inpoly function which is more accurate then polygon approximation. Hot spot max extent maps were also added to include heat maps of the maximum extent of the cells to provide an overall heat map. Trail Selector was updated to include both

--- post_AQuA/Movie-wise Analysis/areaCompare.m
+++ post_AQuA/Movie-wise Analysis/areaCompare.m
@@ -1,7 +1,7 @@
-function ratio = areaCompare(res , bd0 , whichevt , savePath , saveName)
+function ratio = areaCompare(res , whichevt , savePath , saveName)
 %Extract the percentage of the area of an event inside the landmark Input the AQuA result, landmark structure and event number
-if ~exist('res','var') || ~exist('bd0','var')
-    error('No res structure or landmark structure found. Load an AQuA mat file and a landmark file to begin');
+if ~exist('res','var')
+    error('No res structure structure found. Load an AQuA mat file to begin');
 end
 
 if nargin >= 2; set(0,'DefaultFigureVisible','off'); end
@@ -14,38 +14,36 @@ if ~isnumeric(whichevt)
     insideSomaArea = zeros(size(res.evt));
 end
 
-%% Area Calculation
+%% Initialize
 %Calculates number of shared pixels shared between the trace of an event
 %and a frame with only true values (trueFrame) within the borders of
 %landmark
 count = 1;
-landMk = bd0{1}{1}{1};
 frameSize = [size(res.datOrg,1) , size(res.datOrg,2)];
 frameElement = frameSize(1)*frameSize(2);
+landmarkmask = res.fts.region.landMark.mask{1,1};
+areaofsoma = res.opts.spatialRes * sum(sum(landmarkmask));
+picsize = size(res.datOrg);
+%% Area Calculation
 for ii = whichevt
-    %Find trace of Event
-    firstFrame = floor(min(res.evt{(ii)})/frameElement); %First frame of event in movie
-    lastFrame = ceil(max(res.evt{(ii)})/frameElement); %Last frame of event in movie
-    vidFrames = false(frameSize(1) , frameSize(2) , lastFrame-firstFrame);
-    vidFrames(res.evt{(ii)} - firstFrame*frameElement) = true; %Binary movie of event
-    trace = logical(sum(vidFrames,3)); %Trace of event (one frame)
-    %Area
-    [yInd , xInd] = find(trace == true); %x,y indexes of active pixels
-    in = inpolygon(xInd,yInd,landMk(:,2),landMk(:,1));
-    trueFrame = true(size(trace));%dummy variable for true comparison
-    [yTrue , xTrue] = find(trueFrame == true);
-    pin = inpolygon(yTrue,xTrue,landMk(:,2),landMk(:,1));
-    coveredSomaArea(count) = 100 * numel(find(in==true))/numel(find(pin==true)); %Area of soma covered by an event
-    insideSomaArea(count) = 100 * numel(find(in==true))/numel(find(trace==true)); %Area of event inside the soma
-    count = count+1;
+evtXYpix = [];
+somaint = [];
+areaint = [];
+evtXYpix = res.fts.loc.x2D{(ii)};
+evtMap = zeros(picsize(1),picsize(2));
+evtMap(evtXYpix) = 2;
+somaint = evtMap + landmarkmask;
+areaint = evtMap - landmarkmask;
+somacovered(ii)  = (nnz(somaint==3)./nnz(landmarkmask)).*100; %percentage of soma covered by soma
+areacovered(ii)  = (nnz(areaint==1)./nnz(evtMap)).*100; %percentage of event covered by soma
 end
 
 %% Ratio Calculation
-% Ratio of yellow events (completely inside soma)smaller than 200um2 divided
-% by all events under 200um2
+% Ratio of events (completely inside soma)smaller than 50um2 divided
+% by all events under 50um2 
 if ~oneEvent
-    significantEvents = all([insideSomaArea>90;res.fts.basic.area<50;coveredSomaArea>2]);
-    ratio = sum(significantEvents)/sum(all([res.fts.basic.area<50;coveredSomaArea>2]));
+    significantEvents = all([areacovered==100;res.fts.basic.area<50;somacovered>10]);
+    ratio = sum(significantEvents)/sum(all([res.fts.basic.area<50;somacovered>10]));
 end
 
 %% Figure Display
@@ -53,9 +51,9 @@ fig = figure;
 ax = axes('parent',fig);
 if oneEvent %Display only one event and the soma border
     fprintf('Percentage of Soma Covered = %2.2f%%\nPercentage of Trace in Soma = %2.2f%%\n',...
-        coveredSomaArea,insideSomaArea);
+        somacovered,areacovered);
     outText = sprintf('Percentage of Soma Covered = %2.2f%%\nPercentage of Event inside Soma = %2.2f%%\n',...
-        coveredSomaArea,insideSomaArea);
+        somacovered,areacovered);
     imshow(trace);
     hold(ax,'on');
     plot(ax,landMk(:,2),landMk(:,1),'g','LineWidth',2);
@@ -68,17 +66,17 @@ if oneEvent %Display only one event and the soma border
 else %Display chart with colors and calculate ratio    
     h = parula;
     colormap parula
-    color = ceil(63*(insideSomaArea/100))+1;
-    for k = 1:length(coveredSomaArea)
-        plot(ax,res.fts.basic.area(k) , coveredSomaArea(k),...
+    color = ceil(63*(areacovered/100))+1;
+    for k = 1:length(somacovered)
+        plot(ax,res.fts.basic.area(k) , somacovered(k),...
             'Marker','.' ,'MarkerSize',14, 'Color',h(color(k),:));
         hold on
     end
     hold off
-    title(ax,sprintf('Soma Area Representation Ratio=%.6f',ratio));
+    title(ax,{sprintf('Soma Area Representation Ratio=%.6f',ratio),sprintf('Total Area of Soma = %d microns^2', areaofsoma)});
     xlabel(ax,'Size of Event (\mum^2)');
     ylabel(ax,'%Area of Soma Covered');
-    axis(ax , [0 240 2 102]);
+%     axis(ax , [0 240 2 102])
     c = colorbar(ax);
     title(c,'%Event Inside Soma');
     c.TickLabels = num2cell(0:10:100);
---
+++ post_AQuA/Movie-wise Analysis/hotSpotMax.m
@@ -0,0 +1,31 @@
+%% Max Extent of events Hot Spot Map
+function hotSpotMax(res , savePath , saveName)
+mnIm = mean(res.datOrg,3);
+picsize = size(res.datOrg);
+evtMap = zeros(picsize(1),picsize(2));
+totalmap = zeros(picsize(1),picsize(2));
+for evtNumber = 1:length(res.evt)
+evtXYpix = res.fts.loc.x2D{evtNumber};
+evtMap(evtXYpix) = 1;
+totalmap = totalmap + evtMap;
+evtMap = zeros(picsize(1),picsize(2));
+end
+bottom = min(min(totalmap))
+top = max(max(totalmap))
+h(1) = figure('units','normalized');
+imagesc(totalmap); 
+colormap(hot); 
+axis image;
+set(gca,'xtick',[]); set(gca,'ytick',[]);
+title('Max Spatial Extent Heat Map of Cell');
+caxis manual; caxis([bottom top]);
+hcb = colorbar
+set(get(hcb,'label'),'string','Times Pixel is in a Event Maximum spatial footprint');
+%% Saving images
+if nargin >= 2
+    savefig(h , fullfile(savePath,[saveName,'HS_Max.fig']));
+    saveas(h , fullfile(savePath,[saveName,'HS_Max_Map.jpg']));
+    set(0,'DefaultFigureVisible','on');
+end
+set(0,'DefaultFigureVisible','on');
+end
